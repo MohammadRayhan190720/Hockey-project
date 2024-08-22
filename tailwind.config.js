dd65b75a7cbdbdad1b18e6646a33c096ae7c5439
MohammadRayhan190720 <rafirayhan260@gmail.com>
2024-08-22 11:07:28 +0600

    nav section completed successfully

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["*"],
   theme: {
     extend: {
-      
+      fontFamily: {
+        manrope: ["Manrope", "system-ui"],
+      },
     },
   },
   plugins: [],
